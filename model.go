d79be6ab86c94b52ab2d08e98e709826ff5b5f9c
tw <isr910523@gmail.com>
2020-06-27 23:04:56 +0800

    ad extra

--- model.go
+++ model.go
@@ -1,6 +1,10 @@
 package gorm
 
-import "time"
+import (
+	"encoding/json"
+	"log"
+	"time"
+)
 
 // Model base model definition, including fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`, which could be embedded in your models
 //    type User struct {
@@ -10,5 +14,38 @@ type Model struct {
 	ID        int64 `gorm:"primary_key"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	ExtraJson string `gorm:"type:text"`
+	Extra map[string]interface{} `gorm:"-"`
 	DeletedAt *time.Time `sql:"index"`
 }
+
+func (m *Model) SetExtra(p map[string]interface{}) {
+	m.Extra = p
+	if e, err := json.Marshal(p); err != nil {
+		log.Fatalf("model extra set er. p: %v, err: %v", p, err)
+	} else {
+		m.ExtraJson = string(e)
+	}
+}
+
+func (m *Model) GetExtra() map[string]interface{} {
+	if m.Extra == nil {
+		m.Extra = make(map[string]interface{})
+		if m.ExtraJson != "" {
+			if err := json.Unmarshal([]byte(m.ExtraJson), &m.Extra); err != nil {
+				log.Fatalf("model extra unmarshal fail. extraJson: %s", m.ExtraJson)
+			}
+		}
+	}
+	return m.Extra
+}
+
+func (m *Model) AddExtra(key string, value interface{}) {
+	e := m.GetExtra()
+	if oldValue, exist := e[key]; exist {
+		log.Printf("model extra key[%s] override. oldValue: %v, newValue: %v", key, oldValue, value)
+	}
+	e[key] = value
+	m.SetExtra(e)
+}
+
